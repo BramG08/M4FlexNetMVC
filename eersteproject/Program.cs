623b24f8f361d294acb143b6d11a829a6dc25523
BramG08 <bgrippeling08@gmail.com>
2025-05-13 12:05:59 +0200

    Ling Ling

---
+++ eersteproject/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int leeftijd = 20;
+
+string GameChar = "Kirby";
+
+bool isGameOver = false;
+
+float score = 2.6f;
+
+Console.WriteLine("Je leeftijd is: " + leeftijd);
+Console.WriteLine("Je naam is: " + GameChar);
+Console.WriteLine("Is het spel afgelopen? " + isGameOver);
+Console.WriteLine("Je score is: " + score);
