3d60fe8cd67a6856bd67b7eb95a470da03bf29c8
BramG08 <bgrippeling08@gmail.com>
2025-05-20 11:59:10 +0200

    WeatherForecast

--- week2api/weerapi/Models/WeatherForecast.cs
+++ week2api/weerapi/Models/WeatherForecast.cs
@@ -1,7 +1,8 @@
-namespace weerapi;
+namespace weerapi.Models;
 
 public class WeatherForecast
 {
+
     public DateOnly Date { get; set; }
 
     public int TemperatureC { get; set; }
@@ -9,4 +10,5 @@ public class WeatherForecast
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
     public string? Summary { get; set; }
+    public string City { get; set; }
 }
