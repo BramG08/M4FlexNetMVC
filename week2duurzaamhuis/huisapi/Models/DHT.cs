3669668d9b8d77c6ccd7acb695649db1f82b0cd6
BramG08 <bgrippeling08@gmail.com>
2025-05-20 12:54:16 +0200

    Duurzaamhuis

---
+++ week2duurzaamhuis/huisapi/Models/DHT.cs
@@ -0,0 +1,9 @@
+namespace huisapi.Models
+{
+    public class DHT
+    {
+        public int Temperature { get; set; }
+        public int Humidity { get; set; }
+        public int heatIndex { get; set; }
+    }
+}
---
+++ week2duurzaamhuis/huisapi/Models/Sensoren.cs
@@ -0,0 +1,9 @@
+namespace huisapi.Models
+
+{
+    public class Sensor
+    {
+        public int ldr { get; set; }
+        public DHT dht { get; set; }
+    }
+}
