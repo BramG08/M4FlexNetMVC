5833276c163c62b08e1ace18515f3cfa1e7b0c32
BramG08 <bgrippeling08@gmail.com>
2025-05-27 12:17:58 +0200

    Mooie HTML toegevoegd

---
+++ week3razor/newssite/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using newssite.Models;
+
+
+namespace newssite.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        NewsItem item = new NewsItem()
+        {
+            Title = "Mijn eeerste model",
+            Content = "Was niet zo moeilijk om te maken",
+            ImageUrl = "img/newsitem.PNG"
+        };
+        return View(item);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
